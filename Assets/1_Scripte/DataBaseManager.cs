4c233c0b160c6cd5e3f49a7d1691d6784a5014c7
KMS <oinkjwpea@gmail.com>
2024-09-12 12:28:19 +0900

    Initial commit

---
+++ Assets/1_Scripte/DataBaseManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DataBaseManager", menuName = "Scriptable Object/DataBase", order = int.MaxValue)]
+public class DataBaseManager : ScriptableObject
+{
+
+}
